5a9fdc4c89ba9c9230aa89e7aa676259c7ec8969
navneet9971 <Navneetjha2012@gmail.com>
2024-07-18 01:32:04 +0530

    Done navbar

--- frontend/components/SecondNavbar.jsx
+++ frontend/components/SecondNavbar.jsx
@@ -3,12 +3,12 @@ import { miniNavbar } from '@/data/data';
 
 const SecondNavbar = ({ currentScreen, setCurrentScreen }) => {
   return (
-    <header className="bg-white shadow">
-      <div className="flex items-center justify-start gap-20 mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+    <header className="bg-gray-600 shadow">
+      <div className="flex items-center justify-start gap-20 mx-auto max-w-7xl h-6 px-4 py-5 sm:px-6 lg:px-8">
         {miniNavbar.map((option) => (
           <div
             key={option.id}
-            className={`cursor-pointer font-bold ${currentScreen === option.name.toLowerCase() ? 'text-blue-500' : ''}`}
+            className={`cursor-pointer font-bold ${currentScreen === option.name.toLowerCase() ? 'text-blue-500' : 'text-white'}`}
             onClick={() => setCurrentScreen(option.name.toLowerCase())}
           >
             <h1>{option.name}</h1>
