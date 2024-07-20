f67857c6b37a446454d95235e4f613799cf36e87
navneet9971 <Navneetjha2012@gmail.com>
2024-07-20 16:51:56 +0530

    Two section is done

--- frontend/components/ProductComponent/SectionTwo/ProductUser.jsx
+++ frontend/components/ProductComponent/SectionTwo/ProductUser.jsx
@@ -2,19 +2,23 @@
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button";
+import { testData } from "@/data/data";
+import dynamic from 'next/dynamic';
 import React from 'react'
 import { FaUserSecret, FaHandshake } from "react-icons/fa";
 
+// Dynamic import of react-folder-tree with ssr: false
+const FolderTree = dynamic(() => import('react-folder-tree'), { ssr: false });
 
 const ProductUser = () => {
 
-  const hanldeHireDev = () => {
-    alert("Please Hire Mee")
+  const handleHireDev = () => {
+    alert("Please Hire Me")
   }
 
   return (
     <>
-      <div className='w-[27rem] h-full border-black bg-slate-200 '>
+      <div className='w-[27rem] h-full border-black bg-slate-200'>
         <h1 className='bg-blue-400 text-white font-bold text-2xl text-center'>Developer</h1>
 
         <div className='flex flex-col items-center justify-center mt-8 gap-3'>
@@ -23,41 +27,47 @@ const ProductUser = () => {
             <AvatarFallback>Username</AvatarFallback>
           </Avatar>
           <h2 className="font-bold text-md">Username</h2>
-          <FaUserSecret size={25} onClick={hanldeHireDev} className="cursor-pointer" />
-
+          <FaUserSecret size={25} onClick={handleHireDev} className="cursor-pointer" />
 
           <Button variant="destructive" className="flex items-center gap-2">
             <FaHandshake size={20} />
             Negotiate Price
           </Button>
 
-          <Button variant="jump" >
+          <Button variant="jump">
             <FaHandshake size={20} />
-            Request Code Simple
+            Request Code Sample
           </Button>
 
-          <Button variant="jump" >
+          <Button variant="jump">
             <FaHandshake size={20} />
             Seller Support
           </Button>
 
-          <Button variant="jump" >
+          <Button variant="jump">
             <FaHandshake size={20} />
-            Help desk
+            Help Desk
           </Button>
 
           <h1 className="text-sm">User Uploaded Date</h1>
         </div>
       </div>
 
-
       <div className="w-[27rem] h-full border-black bg-slate-200 mt-5">
         <h1 className='bg-blue-400 text-white font-bold text-2xl text-center'>File Tree</h1>
-
+        
+        <div className="flex items-center justify-start ml-5">
+        <FolderTree 
+        data={testData}
+        showCheckbox={false}     
+        showSearch={false}       
+        initOpenStatus="closed"      
+        readOnly ={true} 
+      />
+      </div>
 
 
       </div>
-
     </>
   )
 }
--- frontend/data/data.js
+++ frontend/data/data.js
@@ -248,59 +248,26 @@ export const use = [
     { label: 'Food', value: 'Food' },
 ]
 
-export const ELEMENTS = [
-    {
-      id: "1",
-      isSelectable: true,
-      name: "src",
-      children: [
-        {
-          id: "2",
-          isSelectable: true,
-          name: "app",
-          children: [
-            {
-              id: "3",
-              isSelectable: true,
-              name: "layout.tsx",
-            },
-            {
-              id: "4",
-              isSelectable: true,
-              name: "page.tsx",
-            },
-          ],
-        },
-        {
-          id: "5",
-          isSelectable: true,
-          name: "components",
-          children: [
-            {
-              id: "6",
-              isSelectable: true,
-              name: "header.tsx",
-            },
-            {
-              id: "7",
-              isSelectable: true,
-              name: "footer.tsx",
-            },
-          ],
-        },
-        {
-          id: "8",
-          isSelectable: true,
-          name: "lib",
-          children: [
-            {
-              id: "9",
-              isSelectable: true,
-              name: "utils.ts",
-            },
-          ],
-        },
-      ],
-    },
-  ];
+
+export const testData = {
+    name: "root",
+    children: [
+      {
+        name: "Folder 1",
+        children: [
+          { name: "File 1-1" },
+          { name: "File 1-2" }
+        ]
+      },
+      {
+        name: "Folder 2",
+        children: [
+          { name: "File 2-1" }
+        ]
+      }
+    ]
+  };
+
+
+
   
