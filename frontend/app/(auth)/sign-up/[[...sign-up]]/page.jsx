f1e3901da4edf4ef861f4b8b672dd34cd0bb5fe1
navneet9971 <Navneetjha2012@gmail.com>
2024-07-28 13:18:02 +0530

    Done

--- frontend/app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ frontend/app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -1,20 +1,8 @@
-import AnimatedGridPatter from "@/components/ui/GridPattern";
-import { cn } from "@/lib/utils";
 import { SignUp } from "@clerk/nextjs";
 
 export default function signUp() {
   return (
-    <div className="w-full flex items-center justify-center">
-         <AnimatedGridPatter
-        numSquares={30}
-        maxOpacity={0.1}
-        duration={3}
-        repeatDelay={1}
-        className={cn(
-          "w-full h-screen [mask-image:radial-gradient(500px_circle_at_center,white,transparent)]",
-          "inset-x-0 inset-y-[-30%] h-[200%] skew-y-12",
-        )}
-      />
+    <div className="w-full flex items-center justify-center mb-8">
       <SignUp />
       </div>
   )
